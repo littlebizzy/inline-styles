9bda0ec27b5111f2c47fadbacddbfa0f5a9bbe03
Pau <pauiglesias@gmail.com>
2018-06-29 20:49:55 +0200

    Skip inline functionality for conditional IE stylesheets.

--- styles/inline.php
+++ styles/inline.php
@@ -31,6 +31,13 @@ class Inline extends Helpers\Singleton {
 			if (!in_array($object->handle, $styles->queue))
 				continue;
 
+			// Check conditional IE declaration
+			if (!empty($object->extra['conditional'])) {
+				$conditional = explode(' ', preg_replace('/\s+/', ' ', ''.$object->extra['conditional']));
+				if ('ie' == strtolower($conditional[0]) || (isset($conditional[1]) && 'ie' == strtolower($conditional[1])))
+					continue;
+			}
+
 			// Check valid src
 			if (empty($object->src) || !stripos($object->src, '/wp-content/'))
 				continue;
@@ -44,24 +51,24 @@ class Inline extends Helpers\Singleton {
 			// Retrieve file content
 			$path = WP_CONTENT_DIR.'/'.$src;
 			$content = @file_get_contents($path);
-			if (!empty($content)) {
+			if (empty($content))
+				continue;
 
-				// Convert relative URLs
-				$content = $this->absolutize($content, $src);
+			// Convert relative URLs
+			$content = $this->absolutize($content, $src);
 
-				// Remove reference
-				$object->src = null;
+			// Remove reference
+			$object->src = null;
 
-				// Check extra data
-				if (empty($object->extra) || !is_array($object->extra))
-					$object->extra = [];
+			// Check extra data
+			if (empty($object->extra) || !is_array($object->extra))
+				$object->extra = [];
 
-				// Check previous inline styles
-				$before = isset($object->extra['after'])? (array) $object->extra['after'] : [];
+			// Check previous inline styles
+			$before = isset($object->extra['after'])? (array) $object->extra['after'] : [];
 
-				// Add inline content
-				$object->extra['after'] = array_merge([$content], $before);
-			}
+			// Add inline content
+			$object->extra['after'] = array_merge([$content], $before);
 		}
 	}
 
