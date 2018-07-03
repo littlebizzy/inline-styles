b4700bd6ed9ff36078d72d102d3eda8b8ce0f35f
Jesse Nickles <jesse@littlebizzy.com>
2018-07-03 00:51:54 -0600

    Update admin-notices.php

--- notices/admin-notices.php
+++ notices/admin-notices.php
@@ -26,7 +26,7 @@ final class Admin_Notices {
 	 */
 	private $days_before_display_rate_us = 3; // 3 days delay
 	private $days_dismissing_rate_us = 270; // 9 months reappear
-	private $rate_us_url = 'https://wordpress.org/support/plugin/[plugin uri]/reviews/#new-post';
+	private $rate_us_url = 'https://wordpress.org/support/plugin/inline-styles-littlebizzy/reviews/?rate=5#new-post';
 	private $rate_us_message = 'Thanks for using <strong>%plugin%</strong>. Please support our free work by rating this plugin with 5 stars on WordPress.org. <a href="%url%" target="_blank">Click here to rate us.</a>';
 
 
@@ -37,30 +37,30 @@ final class Admin_Notices {
 	private $days_dismissing_suggestions = 180; // 6 months reappear
 	private $suggestions_message = '%plugin% recommends the following free plugins:';
 	private $suggestions = array(
-		'disable-wc-status-littlebizzy' => array(
-			'name' => 'Disable WooCommerce Status',
-			'desc' => 'Completely disables the WooCommerce Status widget in the WP Admin dashboard to greatly improve backend performance on high traffic WooCommerce shops.',
-			'filename' => 'disable-woocommerce-status.php',
+		'header-cleanup-littlebizzy' => array(
+			'name' => 'Header Cleanup',
+			'desc' => 'Cleans up most of the unnecessary junk meta included by default in the WordPress header including generator, RSD, shortlink, previous and next, etc.',
+			'filename' => 'header-cleanup.php',
+		),
+		'cf-littlebizzy' => array(
+			'name' => 'CloudFlare',
+			'desc' => 'Easily connect your WordPress website to free optimization features from CloudFlare, including one-click options to purge cache and enable dev mode.',
+			'filename' => 'cloudflare.php',
+		),
+		'remove-query-strings-littlebizzy' => array(
+			'name' => 'Remove Query Strings',
+			'desc' => 'Removes all query strings from static resources meaning that proxy servers and beyond can better cache your site content (plus, better SEO scores).',
+			'filename' => 'remove-query-strings.php',
 		),
 		'disable-wc-styles-littlebizzy' => array(
 			'name' => 'Disable WooCommerce Styles',
 			'desc' => 'Completely disables all of the CSS stylesheets that are loaded by WooCommerce in order that styling can be better managed by a single style.css file.',
 			'filename' => 'disable-woocommerce-styles.php',
 		),
-		'delete-expired-transients-littlebizzy' => array(
-			'name' => 'Delete Expired Transients',
-			'desc' => 'Deletes all expired transients upon activation and on a daily basis thereafter via WP Cron to maintain a cleaner database and improve performance.',
-			'filename' => 'delete-expired-transients.php',
-		),
 		'disable-jq-migrate-littlebizzy' => array(
 			'name' => 'Disable jQuery Migrate',
 			'desc' => 'Easily prevent the jQuery migrate script that is included with WordPress core from being loaded to slim down source code (for advanced users only).',
 			'filename' => 'disable-jquery-migrate.php',
-		),
-		'remove-query-strings-littlebizzy' => array(
-			'name' => 'Remove Query Strings',
-			'desc' => 'Removes all query strings from static resources meaning that proxy servers and beyond can better cache your site content (plus, better SEO scores).',
-			'filename' => 'remove-query-strings.php',
 		),
 	);
 
